92daa614194c56ee5d1a1481b0cdc364645fe197
rafeulanamudoy <rafeuludoy@gmail.com>
2023-10-10 13:52:29 +0600

    footer problem fixed

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -5,9 +5,13 @@ import Footer from "./Footer";
 export default function Main() {
   return (
     <div>
-      <Header />
-      <Outlet />
-      <Footer />
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <div className="flex-grow">
+          <Outlet />
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 }
