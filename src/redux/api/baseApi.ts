da619526253bb6d6f565b9d747c5ff5c4384758e
rafeulanamudoy <rafeuludoy@gmail.com>
2023-10-10 13:45:08 +0600

    baseUrl add

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_REACT_APP_API_URl,
+    baseUrl: "https://book-catalog-redux-server.vercel.app/api/v1",
   }),
   tagTypes: ["Book", "updateReview"],
   endpoints: () => ({}),
